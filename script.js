11399735a78db3395fdb3e129b62373c9db77f4f
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-09-25 09:04:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,76 @@
-// complete the js code
 class CustomPromise {
-  // write your code here
+  constructor() {
+    this.status = 'pending';
+    this.value = null;
+    this.reason = null;
+    this.fulfillmentHandlers = [];
+    this.rejectionHandlers = [];
+    this.finallyHandlers = [];
+    
+    const resolve = (value) => {
+      if (this.status === 'pending') {
+        this.status = 'fulfilled';
+        this.value = value;
+        this.fulfillmentHandlers.forEach(handler => handler(value));
+        this.finallyHandlers.forEach(handler => handler());
+      }
+    };
+    
+    const reject = (reason) => {
+      if (this.status === 'pending') {
+        this.status = 'rejected';
+        this.reason = reason;
+        this.rejectionHandlers.forEach(handler => handler(reason));
+        this.finallyHandlers.forEach(handler => handler());
+      }
+    };
+
+    this.resolve = resolve;
+    this.reject = reject;
+  }
+
+  then(onFulfilled, onRejected) {
+    if (this.status === 'fulfilled') {
+      onFulfilled(this.value);
+    } else if (this.status === 'rejected') {
+      onRejected(this.reason);
+    } else {
+      this.fulfillmentHandlers.push(onFulfilled);
+      this.rejectionHandlers.push(onRejected);
+    }
+    return this;
+  }
+
+  catch(onRejected) {
+    return this.then(null, onRejected);
+  }
+
+  finally(onFinally) {
+    if (this.status !== 'pending') {
+      onFinally();
+    } else {
+      this.finallyHandlers.push(onFinally);
+    }
+    return this;
+  }
 }
 
-window.CustomPromise = CustomPromise;
+// Example usage:
+const customPromise = new CustomPromise();
+
+customPromise
+  .then(value => {
+    console.log('Resolved with:', value);
+  })
+  .catch(reason => {
+    console.log('Rejected with:', reason);
+  })
+  .finally(() => {
+    console.log('Finally block executed.');
+  });
+
+// Simulating an asynchronous operation that resolves the promise
+setTimeout(() => {
+  customPromise.resolve('Success!');
+}, 1000);
+
